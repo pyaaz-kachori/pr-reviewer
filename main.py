46b1395cdb61885a0fd413f331352b3fd85ec2f3
NanoNish <nishkarsh1206@gmail.com>
2024-12-07 18:11:13 +0530

    feat: add models

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from types.types import PRModel
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/pr")
-async def check_pr():
+async def check_pr(pr: PRModel):
+    print(pr)
     pass
---
+++ types/types.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import List
+
+class Patch(BaseModel):
+    filename: str
+    raw_url: str
+    patch: str
+
+class Comment(BaseModel):
+    username: str
+    body: str
+
+class Commit(BaseModel):
+    username: str
+    message: str
+    
+class PRModel(BaseModel):
+    url: str
+    title: str
+    contributor: str | None
+    discussions: List[Comment]
+    changes: List[Patch]
+    commits: List[Commit] | None
